28f47581e1cf7fec1bdd139e48a297c2e8b46614
Leesungsup <tmdtjq0116@naver.com>
2023-01-07 23:20:40 +0900

    databinding,npm run lint -- --fix

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,38 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about", webpackPrefetch :true */ '../views/AboutView.vue')
+  },
+  {
+    path: '/databinding/string',
+    name: 'DataBindingStringView',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "databinding", webpackPrefetch :true */ '../views/1_databinding/DataBindingStringView.vue')
+  },
+  {
+    path: '/databinding/html',
+    name: 'DataBindingHtmlView',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "databinding", webpackPrefetch :true */ '../views/1_databinding/DataBindingHtmlView.vue')
+  },
+  {
+    path: '/databinding/Input',
+    name: 'DataBindingInputView',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "databinding", webpackPrefetch :true */ '../views/1_databinding/DataBindingInputView.vue')
+  },
+  {
+    path: '/databinding/select',
+    name: 'DataBindingSelectView',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "databinding", webpackPrefetch :true */ '../views/1_databinding/DataBindingInputView.vue')
   }
 ]
 
---
+++ src/views/1_databinding/DataBindingHtmlView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ htmlString }}</div>
+  <div v-html = 'htmlString'></div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      htmlString: '<p style="color:red;">빨간색 문자</p>'
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
+<style scoped>
+.text-red{
+    color:red;
+}
+</style>
